582ec249edec97f23eb642dffe8d5cbfa1455adc
sjibmach <ibrahim.machlah@gmail.com>
2025-08-04 14:53:23 +0200

    Made it PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import ThemeProviders from '@/providers/theme-provider';
 import ClientOnly from '@/components/client-only';
 import ReactQueryProvider from '@/providers/react-query-provider';
 import { Toaster } from '@/components/ui/sonner';
+import { Metadata } from 'next';
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
@@ -16,11 +17,26 @@ const geistMono = Geist_Mono({
     subsets: ['latin'],
 });
 
-// export const metadata = {
-//     title: 'Dolpro',
-//     description: 'Delpro Workstation',
-// };
-
+export const metadata: Metadata = {
+    title: 'PWA NextJS',
+    description: "It's a simple progressive web application made with NextJS",
+    generator: 'Next.js',
+    manifest: '/manifest.json',
+    keywords: ['nextjs', 'next14', 'pwa', 'next-pwa'],
+    themeColor: [{ media: '(prefers-color-scheme: dark)', color: '#fff' }],
+    authors: [
+        {
+            name: 'imvinojanv',
+            url: 'https://www.linkedin.com/in/imvinojanv/',
+        },
+    ],
+    viewport:
+        'minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover',
+    icons: [
+        { rel: 'apple-touch-icon', url: '/icon-128x128.png' },
+        { rel: 'icon', url: '/icon-128x128.png' },
+    ],
+};
 export default function RootLayout({
     children,
 }: Readonly<{
@@ -28,6 +44,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="de" suppressHydrationWarning>
+            <head>
+                <link rel="manifest" href="/manifest.json" />
+            </head>
             <body
                 className={`${geistSans.variable} ${geistMono.variable} bg-amber-50 antialiased dark:bg-gray-950`}
             >
